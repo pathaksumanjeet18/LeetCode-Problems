9626bd8f94ed2e4401258ac8416279633d3b0fbf
Sumanjeet Pathak <65484300+pathaksumanjeet18@users.noreply.github.com>
2022-06-18 09:23:15 +0530

    Time: 0 ms (100.00%), Space: 6.1 MB (86.40%) - LeetHub

--- 796-rotate-string/796-rotate-string.cpp
+++ 796-rotate-string/796-rotate-string.cpp
@@ -3,16 +3,21 @@ public:
     bool rotateString(string s, string goal) {
         if(s==goal)
             return true;
-        int i = 0;
-        while(i<s.length()){
-            string ex = "";
-            ex += s[s.length()-1];
-            s.pop_back();
-            s = ex+s;
-            if(s==goal)
-                return true;
-            i++;
-        }
+        if(s.length()!=goal.length())
+            return false;
+        // int i = 0;
+        // while(i<s.length()){
+        //     string ex = "";
+        //     ex += s[s.length()-1];
+        //     s.pop_back();
+        //     s = ex+s;
+        //     if(s==goal)
+        //         return true;
+        //     i++;
+        // }
+        
+        string temp = s+s;
+        return temp.find(goal)!=string::npos?true:false;
         return false;
     }
 };
