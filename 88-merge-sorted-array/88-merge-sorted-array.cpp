a252b8454e262961562bc7d77bd56f8656466093
Sumanjeet Pathak <65484300+pathaksumanjeet18@users.noreply.github.com>
2022-06-24 22:51:10 +0530

    Time: 4 ms (51.91%), Space: 9.1 MB (29.14%) - LeetHub

--- 88-merge-sorted-array/88-merge-sorted-array.cpp
+++ 88-merge-sorted-array/88-merge-sorted-array.cpp
@@ -3,7 +3,16 @@ public:
     void merge(vector<int>& nums1, int m, vector<int>& nums2, int n) {
         int i = m-1;int j = n-1;
         int k = m + n - 1;
-        
+        if(n==0)
+            return;
+        int l = 0;
+        if(m==0){
+            for(int i = 0;i<n;i++){
+                nums1[l] = nums2[i];
+                l++;
+            }
+            return;
+        }
         while(i>=0 and j>=0){
             
             if(nums2[j]>=nums1[i]){
