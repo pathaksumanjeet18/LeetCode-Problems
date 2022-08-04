3b7d72e1c47896fa90d42208094f47ff5271b2cc
Sumanjeet Pathak <65484300+pathaksumanjeet18@users.noreply.github.com>
2022-08-04 22:30:57 +0530

    Time: 47 ms (5.10%), Space: 5.9 MB (73.92%) - LeetHub

--- 69-sqrtx/69-sqrtx.cpp
+++ 69-sqrtx/69-sqrtx.cpp
@@ -1,7 +1,18 @@
 class Solution {
 public:
     int mySqrt(int x) {
-        int y = sqrt(x);
-        return y;
+        long long i = 0;int ans = 0;
+        while(i<=x){
+            if(((long long)(i*i))==x){
+                return i;
+            }
+            else if(((long long)(i*i)) >x){
+                return (i-1);
+            }
+            else{
+                i++;
+            }
+        }
+        return i;
     }
 };
