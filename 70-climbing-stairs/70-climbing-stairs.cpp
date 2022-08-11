213fe9f8fa5f5e225411cd9c9754625caeba2518
Sumanjeet Pathak <65484300+pathaksumanjeet18@users.noreply.github.com>
2022-08-11 17:40:30 +0530

    Time: 5 ms (12.63%), Space: 6 MB (56.91%) - LeetHub

--- 70-climbing-stairs/70-climbing-stairs.cpp
+++ 70-climbing-stairs/70-climbing-stairs.cpp
@@ -1,28 +1,15 @@
 class Solution {
 public:
-    
     int climbStairs(int n) {
-        long long int dp[100];
-        for(int i = 0;i<100;i++)
-            dp[i] = -1;
-
-        return ans(n,dp);
-
-    }
-    long long int ans(int n,long long int dp[]){
-         if(n==0 ){
-            dp[n] = 1;
-            return 1;
+         int t[n+1];
+        t[0] = 0;
+         for(int i = 1;i<=n;i++){
+             if(i<=2)
+                 t[i] = i;
+             else{
+                 t[i] = t[i-1]+t[i-2];
+             }
          }
-         if(n<0)
-            return 0;
-         if(dp[n]!=-1)
-             return dp[n];
-        else{
-            long long int x = ans(n-1,dp) + ans(n-2,dp);
-            dp[n] = x;
-        }
-        return dp[n];
+        return t[n];
     }
-    
 };
